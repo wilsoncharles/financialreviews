93673dd5c098afa6cd45615168c6b22871bc1b09
wilsoncharles <wilsoncharles21@gmail.com>
2022-11-23 04:29:03 +0530

    validation 2nd commit

--- finance_complaint/component/training/data_validation.py
+++ finance_complaint/component/training/data_validation.py
@@ -3,7 +3,8 @@ import sys
 from collections import namedtuple
 from typing import List, Dict 
 from pyspark.sql import DataFrame
-
+from pyspark.sql.functions import lit
+from pyspark.sql.functions import col
 
 from finance_complaint.exception import FinanceException
 from finance_complaint.logger import logger 
@@ -67,4 +68,95 @@ class DataValidation(FinanceDataSchema):
         except Exception as e:
             raise FinanceException(e,sys)
 
+    def get_unwanted_and_high_missing_value_columns(self, dataframe: DataFrame, threshold:float = 0.2) ->List[str]:
+        try:
+            missing_report : Dict[str, MissingReport] = self.get_missing_report(dataframe = dataframe)
+            unwanted_column: List[str] = self.schema.unwanted_columns
+
+            for column in missing_report:
+                if missing_report[column].missing_percentage > (threshold*100):
+                    unwanted_column.append(column)
+                    logger.info(f"Missing report {column} : [{missing_report[column]}]")
+            unwanted_column = list(set(unwanted_column))
+            return unwanted_column
+        except Exception as e:
+            raise FinanceException(e,sys)
     
+    def drop_unwanted_columns(self, dataframe: DataFrame) -> DataFrame:
+        try:
+            unwanted_columns : List = self.get_unwanted_and_high_missing_value_columns(dataframe = dataframe)
+            logger.info(f"Dropping feature: {','.join(unwanted_columns)}")
+            unwanted_dataframe: DataFrame = dataframe.select(unwanted_columns)
+
+            unwanted_dataframe = unwanted_dataframe.withColumn(ERROR_MESSAGE, lit("Contains many missing values"))
+
+            rejected_dir = os.path.join(self.data_validation_config.rejected_data_dir, "missing_data")
+            os.makedirs(rejected_dir, exist_ok = True)
+            file_path = os.path.join(rejected_dir, self.data_validation_config.file_name)
+
+            logger.info(f"Writing dropped column into file: [{file_path}]")
+            unwanted_dataframe.write.mode("append").parquet(file_path)
+            dataframe: DataFrame = dataframe.drop(*unwanted_columns)
+            logger.info(f"Remaining number of columns: [{dataframe.columns}]")
+            return dataframe
+
+        except Exception as e:
+            raise FinanceException(e,sys)
+
+        
+    @staticmethod
+    def get_unique_values_of_each_column(dataframe: DataFrame) -> None:
+        try:
+            for column in dataframe.columns:
+                n_unique: int = dataframe.select(col(column)).distinct.count()
+                n_missing: int = dataframe.filter(col(column).isnull()).count()
+                missing_percentage :float = (n_missing * 100)/dataframe.count()
+                logger.info(f"Column: {column} contains {n_unique} value and missing perc: {missing_percentage} %.")
+        except Exception as e:
+            raise FinanceException(e,sys)     
+
+    
+    def is_required_columns_exist(self, dataframe: DataFrame):
+        try:
+            columns = list(filter(lambda x: x in self.schema.required_columns,
+                                  dataframe.columns))
+
+            if len(columns) != len(self.schema.required_columns):
+                raise Exception(f"Required column missing\n\
+                 Expected columns: {self.schema.required_columns}\n\
+                 Found columns: {columns}\
+                 ")
+
+        except Exception as e:
+            raise FinanceException(e, sys)
+
+    def initiate_data_validation(self) -> DataValidationArtifact:
+
+        try:
+            logger.info(f"Initiating data preprocessing")
+            dataframe : DataFrame = self.read_data()
+
+            logger.info(f"Drop unwanted columns")
+            dataframe : DataFrame = self.drop_unwanted_columns(dataframe = dataframe)
+
+            self.is_required_columns_exist(dataframe = dataframe)
+
+            print(f"Row: [{dataframe.count()}] Column: [{len(dataframe.columns)}]")
+            print(f"Expected Column: {self.schema.required_columns}\nPresent Columns: {dataframe.columns}")
+
+            os.makedirs(self.data_validation_config.accepted_data_dir, exist_ok=True)
+            accepted_file_path = os.path.join(self.data_validation_config.accepted_data_dir,
+                                              self.data_validation_config.file_name
+                                              )
+            dataframe.write.parquet(accepted_file_path)
+
+            artifact = DataValidationArtifact(accepted_file_path=accepted_file_path,
+                                              rejected_dir=self.data_validation_config.rejected_data_dir
+                                              )
+            logger.info(f"Data validation artifact: [{artifact}]")
+            return artifact
+
+
+
+         except Exception as e:
+            raise FinanceException(e, sys)
